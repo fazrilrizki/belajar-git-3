0638ae2a7447ef43290a927f7e0dcb4257f1f7fa
fazrilrizki <fazrilrizkitantoadji@gmail.com>
2023-05-03 11:55:20 +0700

    Mencoba branch conflicts

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::get('/welcome2', function () {
+    return view('welcome2');
+});
+
 Route::get('/welcome', function () {
     return view('welcome');
 });
